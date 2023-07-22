f05876ca6029e35faff1c66c6f69b0fe96f78456
Ivan Živolić <70758717+ivegoie@users.noreply.github.com>
2023-07-22 19:45:31 +0200

    Implemented UI

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import tkinter as tk
 from tkinter import font
 
 
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Basic Calculator")
+        self.root.resizable(False, False)
+        
+
+def main():
+    app = GUI()
+    app.root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
